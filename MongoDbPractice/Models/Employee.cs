f27b16b797d6d74bba34bb49c8ee10f991eaf989
vasanthakumar <jmvasanthakumar@gmail.com>
2022-07-27 17:17:13 +0530

    Add project files.

---
+++ MongoDbPractice/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbPractice.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Email { get; set; }
+    }
+}
